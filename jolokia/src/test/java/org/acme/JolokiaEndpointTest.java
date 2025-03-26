bcbf565ab6ecd8894530673a556ac760dad8a52c
James Netherton <jamesnetherton@gmail.com>
2025-03-26 13:46:57 +0000

    Add jolokia demo

---
+++ jolokia/src/test/java/org/acme/JolokiaEndpointTest.java
@@ -0,0 +1,25 @@
+package org.acme;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import org.apache.camel.quarkus.jolokia.restrictor.CamelJolokiaRestrictor;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.matchesPattern;
+
+@QuarkusTest
+class JolokiaEndpointTest {
+    @Test
+    void jolokiaConfiguration() {
+        RestAssured.get("/q/jolokia")
+                .then()
+                .statusCode(200)
+                .body(
+                        "status", equalTo(200),
+                        "value.config.discoveryEnabled", equalTo("true"),
+                        "value.config.restrictorClass", equalTo(CamelJolokiaRestrictor.class.getName()),
+                        "value.config.agentDescription", equalTo("camel-quarkus-jolokia-demo"),
+                        "value.details.url", matchesPattern("http://.*:8778/jolokia/"));
+    }
+}
